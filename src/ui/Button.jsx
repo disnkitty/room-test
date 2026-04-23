e85e34f7712fc8d6c6262428d66bd9112995c0d9
Chirka Liubov <chirka19982007@gmail.com>
2026-04-23 22:59:40 +0300

    variants

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,5 +5,9 @@ function Button({ type = 'button', className = '', onClick, children, ...rest })
     </button>
   );
 }
-
+// const variants = {
+//   primary: "bg-[#EAFF00] text-[#1A181C] w-[262px]", 
+//   secondary: "bg-[#3A3A3A] text-white w-[262px]",   
+//   ghost: "bg-transparent text-[#939393] w-auto h-auto p-0", 
+// };
 export default Button;
